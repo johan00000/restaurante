48301317e60a010da0b8884206f4f165268b120a
johan <johanorozco496@gmail.com>
2024-08-25 16:25:34 -0500

    se configura servidor node con validacion de 2 url

--- controller/app.js
+++ controller/app.js
@@ -1,1 +1,27 @@
-console.log('hola mundo como te vasejecutando el proyecto')
+const { json } = require('express')
+const { access } = require('fs')
+const http= require('http')
+const port=5000
+const host="localhost"
+
+const server =http.createServer((request,response)=>{
+    response.end('hola mundo')
+
+    try{
+        if(request.method == "GET" && request.url=="/"){
+            response.end(JSON.stringify({"view":"1"}))
+
+        } else if(request.method == "GET" && request.url=="vista1"){
+            response.end(JSON.stringify({"userStatus":"access"}))
+
+        }
+    }catch{
+        response.end("la ruta no esta encontrada")
+    }
+})
+
+
+
+server.listen(5000,()=>{
+    console.log(`se esta ejecutando en http//:${host}:${port}`)
+})
